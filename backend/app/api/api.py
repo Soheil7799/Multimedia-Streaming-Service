f759f2979684b7d183c81ebadcbae76675ebc8fa
Mostafa Shirvani <MostafaShirvani1377@outlook.com>
2025-04-07 20:43:06 +0200

    Update api.py
    to include analysis

--- backend/app/api/api.py
+++ backend/app/api/api.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
-from app.api.endpoints import upload, filters, streaming
+from app.api.endpoints import upload, filters, streaming, analysis
 
 api_router = APIRouter()
 
 api_router.include_router(upload.router, prefix="/upload", tags=["upload"])
 api_router.include_router(filters.router, prefix="/filters", tags=["filters"])
 api_router.include_router(streaming.router, prefix="/stream", tags=["streaming"])
+api_router.include_router(analysis.router, prefix="/analyze", tags=["analysis"])
